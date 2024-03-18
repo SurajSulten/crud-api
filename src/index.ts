1cd5e35e2181a6eb6285fa86c42ee5d25101d55e
Suroj <suraj.sulten@gmail.com>
2024-03-18 10:50:17 +0500

    create methods

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,30 @@ const PORT = process.env.PORT ?? 3001
 
 // create a server object:
 const server = http.createServer((req, res) => {
-    
     // const dataJson = fs.readFileSync("data.json", { encoding: "utf-8" });
     // const database = JSON.parse(dataJson);
-
-    res.setHeader("Content-type", "application/json");  
-
-    console.log(req.url);
+    switch (req.method) {
+        case "GET":
+            getReq(req, res);
+            break;
+        case "POST":
+            postReq(req, res);
+            break;
+        case "PUT":
+            putReq(req, res);
+            break;
+        case "DELETE":
+            deleteReq(req, res);
+            break;
+    
+        default:
+            res.statusCode = 404;
+            res.setHeader("Content-type", "application/json");  
+            res.write(JSON.stringify({title: "Not found", message: 'Route not found'}));
+            res.end();
+            break;
+    }
+    
    
 }); 
 
