7f01be6689ca4f764e717d1fc3b28245e48c1498
Suroj <suraj.sulten@gmail.com>
2024-03-18 10:30:37 +0500

    feat: create server

--- src/getUsers.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-
-export function getUsers(req, res) {
-    const dataJson = fs.readFileSync("data.json", { encoding: "utf-8" });
-    const database = JSON.parse(dataJson);
-
-    res.write(JSON.stringify({ users: database.users }));
-    res.end();
-}
