caa5740aec33109967c8bbd80edc39427c0c9504
Suroj <suraj.sulten@gmail.com>
2024-03-21 00:38:15 +0500

    feat: implement delete req

--- src/methods/delete-request.ts
+++ src/methods/delete-request.ts
@@ -1,1 +1,28 @@
-// export function deleteReq(res, req): void {}
+import writeToFile from "../util/write-to-file";
+
+export function deleteReq(res, req): void {
+    const baseUrl = req.url.substring(0, req.url.lastIndexOf("/") + 1);
+    const id = req.url.split("/")[3];
+    // const regexV4 = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i); 
+    const regexV4 = /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
+
+    if(!regexV4.test(id)) {
+        res.writeHead(400, {"Content-type": "application/json"});
+        res.end(JSON.stringify({title: "Validation failed", message: 'Not valid uuid'}))
+    } else if(baseUrl === "/api/users/" && regexV4.test(id)) {
+        const index = req.users.findIndex((user) => {
+            return user.id === id
+        });
+        if(index === -1) {
+            res.statusCode = 404;
+            req.write(JSON.stringify({title: "Not found", message: 'User not found'}));
+            res.end();
+        } else {
+            req.users.splice(index, 1);
+            writeToFile(req.users);
+            res.writeHead(204, {"Content-type": "application/json"});
+            res.end(JSON.stringify(req.users));
+        }
+    }
+}
+
